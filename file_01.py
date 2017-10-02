d7e9198cafce38498b73e0006d358e036b41b0a0
kotpilun <31470203+kotpilun@users.noreply.github.com>
2017-10-02 23:00:39 +0300

    done + good solution + super

--- file_01.py
+++ file_01.py
@@ -6,44 +6,44 @@
 # В исходном тексте не встречаются цифры, так что код однозначно интерпретируем.
 #
 
-
-def char_int(x):
-    result = ''
-    count = 1
-    dig = ''
-    f = False
-    i = 0
-    #for i in range(len(x)):
-    while i < len(x):
-        if x[i] in '1234567890': #если x[i] - число
-            count = 0
-            while (x[i] in '1234567890'): # идем дальше, ищем числа
-                if count == 0:
-                    j = i - 1  # запоминаем каким было последнее и с буквой
-                count += 1 #считаем количество чисел
-                dig += str(x[i]) #запоминаем цифру в виде строки для соединения нескольких цифр в одну
-                if i < len(x)-1: #проверяем на выход из диапазона списка
-                    i += 1
-                else:
-                    f = True # дошли до конца. выходить из for.
-                    break
-            result += str(x[j]) * int(dig)
-            dig = ''
-        if f == True:
-            break
-        i += 1
-    return result
-
-# a = input()
-# a = list(a)
-with open('F:\Py_projects\Stepic_course\datasets\dataset_3363_2.txt') as inf:
-     for line in inf:
-         line = line.strip()
-         a = list(line)
-# print(line)
-# print(char_int(a))
-with open('F:\Py_projects\Stepic_course\datasets\out.txt', 'w') as outf:
-    outf.write(char_int(a))
+#
+# def char_int(x):
+#     result = ''
+#     count = 1
+#     dig = ''
+#     f = False
+#     i = 0
+#     #for i in range(len(x)):
+#     while i < len(x):
+#         if x[i] in '1234567890': #если x[i] - число
+#             count = 0
+#             while (x[i] in '1234567890'): # идем дальше, ищем числа
+#                 if count == 0:
+#                     j = i - 1  # запоминаем каким было последнее и с буквой
+#                 count += 1 #считаем количество чисел
+#                 dig += str(x[i]) #запоминаем цифру в виде строки для соединения нескольких цифр в одну
+#                 if i < len(x)-1: #проверяем на выход из диапазона списка
+#                     i += 1
+#                 else:
+#                     f = True # дошли до конца. выходить из for.
+#                     break
+#             result += str(x[j]) * int(dig)
+#             dig = ''
+#         if f == True:
+#             break
+#         i += 1
+#     return result
+#
+# # a = input()
+# # a = list(a)
+# with open('F:\Py_projects\Stepic_course\datasets\dataset_3363_2.txt') as inf:
+#      for line in inf:
+#          line = line.strip()
+#          a = list(line)
+# # print(line)
+# # print(char_int(a))
+# with open('F:\Py_projects\Stepic_course\datasets\out.txt', 'w') as outf:
+#     outf.write(char_int(a))
 
 #####################################################################
 #Это решение понравилось
@@ -56,3 +56,10 @@ with open('F:\Py_projects\Stepic_course\datasets\out.txt', 'w') as outf:
 #         j += 1
 #     print(s[i] * int(s[i+1:j]), end='')
 #     i = j
+######################################################
+#GREAT!
+s, d = input(), []
+for i in s:
+    if not i.isdigit(): d.append(i)
+    else: d[-1] += i
+print(*[i[0]*int(i[1:]) for i in d], sep='')
